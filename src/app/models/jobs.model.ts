74d7edd5e71cc0694998d10bcd3ce61b304ae11c
Greg Van Dell <gvandell@engineation.com>
2023-05-26 14:37:44 -0400

    recent jobs page working

--- src/app/models/jobs.model.ts
+++ src/app/models/jobs.model.ts
@@ -2,16 +2,16 @@ export interface Job {
     jobId: number;
     jobBoardId: number;
     jobName: string | null;
-    Location: string | null;
-    Company: string | null;
-    ShortDesc: string | null;
-    URL: string | null;
-    Salary: string | null;
-    WorkType: string | null;
-    Skills: string | null;
-    JobDescription: string | null;
-    DateTimeFetched: Date;
-    AppliedTo: boolean;
-    CoverLetter: string | null;
-    DateTimeApplied: Date | null;
+    location: string | null;
+    company: string | null;
+    shortDesc: string | null;
+    url: string | null;
+    salary: string | null;
+    workType: string | null;
+    skills: string | null;
+    jobDescription: string | null;
+    dateTimeFetched: Date;
+    appliedTo: boolean;
+    coverLetter: string | null;
+    dateTimeApplied: Date | null;
 }
